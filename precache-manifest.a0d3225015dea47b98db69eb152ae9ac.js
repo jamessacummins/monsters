93bb0a1499a587822bf0ecccd93e21bc6d6fc315
jamessacummins <jamessacummins@gmail.com>
2020-08-28 17:12:34 +1000

    Updates

---
+++ precache-manifest.a0d3225015dea47b98db69eb152ae9ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323e9ed456226986b3f4737c64440e91",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "45b7d0dc534060737b84",
+    "url": "/monsters/static/css/main.98cab239.chunk.css"
+  },
+  {
+    "revision": "1de04655a7d62103a81b",
+    "url": "/monsters/static/js/2.77954618.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/monsters/static/js/2.77954618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45b7d0dc534060737b84",
+    "url": "/monsters/static/js/main.5cebe535.chunk.js"
+  },
+  {
+    "revision": "3dab8aa598718cb8e8b3",
+    "url": "/monsters/static/js/runtime-main.f48c3991.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters/static/media/logo.5d5d9eef.svg"
+  }
+]);
